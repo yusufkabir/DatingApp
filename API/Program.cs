250f97a4daa5b5f85302f3042d85ee6f6a23c6ae
Yusufkabir <yusufkabir@hotmail.co.uk>
2023-01-23 19:26:46 +0000

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
