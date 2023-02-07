7b3aef4bb7d7536224f9dee90d708e4633d7b9d4
Yusufkabir <yusufkabir@hotmail.co.uk>
2023-02-07 22:11:33 +0000

    End of Section 12

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -82,6 +82,11 @@ deletePhoto(photoId: number){
       if(response){
         const photo = JSON.parse(response);
         this.member?.photos.push(photo);
+        if(photo.isMain && this.user && this.member){
+          this.user.photoUrl = photo.url;
+          this.member.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+        }
       }
     }
   }
